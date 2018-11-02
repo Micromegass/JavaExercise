68606654437b7b1b7162be41ca9555721e713377
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-11-02 18:30:44 -0500

    smallest common denominator

---
+++ src/ggt.java
@@ -0,0 +1,70 @@
+
+public class ggt {
+	
+	
+	
+	public static void main(String args[]) {
+		
+		int s1 = Integer.parseInt(args[0]);
+		int s2 = Integer.parseInt(args[1]);
+
+		int x = 0; 
+		int rest = 0; 
+		int ggt = 0; 
+		
+		
+		if (s1 > s2) {
+			
+			while (s1%s2 != 0) {
+				x = s1/s2;
+				rest = s1% s2;
+				s1 = s2; 
+				s2 = rest; 	
+				ggt = s2;
+				
+			}	
+			
+
+			
+		} else {
+			
+			
+			while (s2%s1 != 0) {
+				x = s2/s1;
+				rest = s2% s1;
+				s2 = s1; 
+				s1 = rest; 	
+				ggt = s1;
+
+			
+			
+		}
+		
+
+		
+		
+		}
+		
+				
+		
+		switch (ggt) {
+		case 1: 
+		case 2: 
+		case 3: 
+		case 4: 
+			System.out.print("Smallest common denominator is bewtween 1 and 4. It is: " + ggt);
+			break; 
+		default:
+			System.out.print("Smallest common denominator is not bewtween 1 and 4. It is: " + ggt);
+
+		}
+		
+		
+		
+	}
+		
+		
+}
+	
+
+
---
+++ src/schleifen.java
@@ -0,0 +1,26 @@
+
+public class schleifen {
+	
+	
+	public static void main(String args[]) {
+		
+		int i = 0; 
+
+		if (args.length > 0) {
+	
+		do  {
+		System.out.println(args[i]);
+		++i;
+		}
+		
+		while (i < args.length);
+			
+			
+		}
+		
+		
+		
+	}
+	
+
+}
