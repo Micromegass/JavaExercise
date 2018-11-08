36323feb9815fe9c565bbbcd4a710df8a913066b
axelbraunschweiger <axelbraunschweiger@gmail.com>
2018-11-08 17:57:36 -0500

    typo

--- src/FractionTest.java
+++ src/FractionTest.java
@@ -2,7 +2,8 @@
 public class FractionTest {
 	
 	
-	public static void main(String[] args) {
+	
+public static void main(String[] args) {
 		
 		Fraction f1 = new Fraction(2,3); 
 		Fraction f2 = new Fraction(3,1); 
@@ -11,12 +12,11 @@ public class FractionTest {
 		
 		System.out.println(f1.multiplyBy(f2));
 		System.out.println(f1.toString());
-
+	    System.out.println(Math.PI);;
+	    double zehnHochZwei = Math.pow(2, 10);
+	    System.out.println(zehnHochZwei);
 		
 	}
-	
-	
-
 	
 
 	
---
+++ src/sortLib.java
@@ -0,0 +1,39 @@
+
+
+public class sortLib {
+	
+	
+
+	public static void main (String[] args) {
+		
+		
+		int[] array = {12,23,35,4657,67,5656,1}; 
+	
+	
+		
+		java.util.Arrays.sort(array); 
+		
+		for (int elem:array) {
+			
+			System.out.println(elem);
+		}
+		
+		
+		System.out.println(java.util.Arrays.toString(array));
+		
+		
+		  String s1 = String.valueOf(3.0 / 4.0);
+	      System.out.println(s1);
+	      String s2 = String.valueOf("Test");
+	      String s3 = String.valueOf("Test");
+	      System.out.println(s2 == s3);
+	      String s4 = new String("Test");
+	      System.out.println(s2.equals(s4));
+	    	
+
+	}	
+	
+	
+
+
+}
